4e9131561e71dc5521e2bda0dfde3383598c18ea
n0ne <n0ne@noreply.codeberg.org>
2026-06-02 17:59:14 +0000

    feat: add shorthand flag for preset (-p)

--- cmd/root.go
+++ cmd/root.go
@@ -48,7 +48,7 @@ func init() {
 	cobra.OnInitialize(initConfig)
 
 	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.config/parsec/config.toml)")
-	rootCmd.PersistentFlags().StringVar(&presetFlag, "preset", "", "configuration preset to use")
+	rootCmd.PersistentFlags().StringVarP(&presetFlag, "preset", "p", "", "configuration preset to use")
 	rootCmd.PersistentFlags().BoolVar(&debugFlag, "debug", false, "enable debug output")
 	rootCmd.PersistentFlags().BoolVar(&noCacheFlag, "no-cache", false, "bypass the API cache and fetch fresh data")
 
